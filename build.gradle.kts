a4459622acef24e50562d4617365dabc511a01e4
Rahul Somasunderam <rahul.som@gmail.com>
2019-03-26 21:59:12 -0700

    Enable release to bintray with circleci

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
+import com.jfrog.bintray.gradle.BintrayExtension
+
 plugins {
     `java-library`
     groovy
     id("nebula.release") version "9.2.0"
     id("nebula.maven-publish") version "9.5.0"
     id("nebula.source-jar") version "9.5.0"
-    id("nebula.test-jar") version "9.5.0"
     id("nebula.javadoc-jar") version "9.5.0"
+    id("nebula.nebula-bintray-publishing") version "5.0.0"
 }
 
 repositories {
@@ -21,4 +23,18 @@ dependencies {
 
     testImplementation("org.spockframework:spock-core:1.2-groovy-2.5")
     testImplementation("junit:junit:4.12")
+}
+
+bintray {
+    user = (System.getenv("BINTRAY_USER"))
+    key = (System.getenv("BINTRAY_KEY"))
+    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+        userOrg = ("rahulsom")
+        repo = ("m2")
+        websiteUrl = ("https://github.com/rahulsom/jansi-table")
+        issueTrackerUrl = ("https://github.com/rahulsom/jansi-table/issues")
+        vcsUrl = ("https://github.com/rahulsom/jansi-table.git")
+        setLicenses("Apache-2.0")
+        setLabels("jansi", "textui", "table")
+    })
 }
