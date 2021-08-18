36c0c20ff7bc567b372cd202a9d42cda805d3bae
Rahul Somasunderam <rahul.som@gmail.com>
2021-08-18 13:43:06 -0700

    Write build scan info to file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,16 @@
+plugins {
+    id("com.gradle.enterprise").version("3.6.3")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+        buildScanPublished {
+            file("build/gradle-scan.md").appendText("""Gradle Build Scan - [`${this.buildScanId}`](${this.buildScanUri})""")
+        }
+    }
+}
+
 rootProject.name = "jansi-table"
