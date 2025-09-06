1a2fe1c4767a38d1a307a5fefef7a87bcd417c20
Rahul Somasunderam <rahulsom@noreply.github.com>
2025-09-05 19:42:00 -0700

    chore: Upgrade JDK/Gradle/Waena
    This takes us to JDK 17, Gradle 9, and, Waena 0.16.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 plugins {
     `java-library`
     groovy
-    id("com.github.rahulsom.waena.root").version("0.6.1")
-    id("com.github.rahulsom.waena.published").version("0.6.1")
+    id("com.github.rahulsom.waena.root").version("0.16.0")
+    id("com.github.rahulsom.waena.published").version("0.16.0")
 }
 
 repositories {
@@ -27,6 +27,9 @@ dependencies {
     implementation("org.fusesource.jansi:jansi:2.4.0")
 
     testImplementation("org.spockframework:spock-core:2.3-groovy-4.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.1")
 }
 
 tasks.withType<Test> {
